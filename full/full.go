5592bd04a9eebdd643b60a98c2ebb4e7bbfdd2ef
Yukiro-Proseko <tuman3352@gmail.com>
2025-02-17 23:13:24 +0400

    Add files via upload

---
+++ full/full.go
@@ -0,0 +1,4 @@
+package full
+func SSD() int {
+	return 666
+}
---
+++ main/main-full.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"fmt"
+	"module/програмирование/full"
+)
+func SSD() int {
+	return 666
+}
+func main() {
+	fmt.Print(SSD())
+}
